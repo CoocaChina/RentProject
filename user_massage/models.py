91e33754a17e3d1c1e935d9d41f36ecb9bea0f9f
wan1101 <728457901@qq.com>
2023-11-20 17:43:33 +0800

    login

--- user_massage/models.py
+++ user_massage/models.py
@@ -1,3 +1,4 @@
+
 # This is an auto-generated Django model module.
 # You'll have to do the following manually to clean this up:
 #   * Rearrange models' order
@@ -6,13 +7,123 @@
 #   * Remove `managed = False` lines if you wish to allow Django to create, modify, and delete the table
 # Feel free to rename the models, but don't rename db_table values or field names.
 from django.db import models
-from datetime import datetime
+
+
+class AuthGroup(models.Model):
+    name = models.CharField(unique=True, max_length=150)
+
+    class Meta:
+        managed = False
+        db_table = 'auth_group'
+
+
+class AuthGroupPermissions(models.Model):
+    group = models.ForeignKey(AuthGroup, models.DO_NOTHING)
+    permission = models.ForeignKey('AuthPermission', models.DO_NOTHING)
+
+    class Meta:
+        managed = False
+        db_table = 'auth_group_permissions'
+        unique_together = (('group', 'permission'),)
+
+
+class AuthPermission(models.Model):
+    name = models.CharField(max_length=255)
+    content_type = models.ForeignKey('DjangoContentType', models.DO_NOTHING)
+    codename = models.CharField(max_length=100)
+
+    class Meta:
+        managed = False
+        db_table = 'auth_permission'
+        unique_together = (('content_type', 'codename'),)
+
+
+class AuthUser(models.Model):
+    password = models.CharField(max_length=128)
+    last_login = models.DateTimeField(blank=True, null=True)
+    is_superuser = models.IntegerField()
+    username = models.CharField(unique=True, max_length=150)
+    first_name = models.CharField(max_length=30)
+    last_name = models.CharField(max_length=150)
+    email = models.CharField(max_length=254)
+    is_staff = models.IntegerField()
+    is_active = models.IntegerField()
+    date_joined = models.DateTimeField()
+
+    class Meta:
+        managed = False
+        db_table = 'auth_user'
+
+
+class AuthUserGroups(models.Model):
+    user = models.ForeignKey(AuthUser, models.DO_NOTHING)
+    group = models.ForeignKey(AuthGroup, models.DO_NOTHING)
+
+    class Meta:
+        managed = False
+        db_table = 'auth_user_groups'
+        unique_together = (('user', 'group'),)
+
+
+class AuthUserUserPermissions(models.Model):
+    user = models.ForeignKey(AuthUser, models.DO_NOTHING)
+    permission = models.ForeignKey(AuthPermission, models.DO_NOTHING)
+
+    class Meta:
+        managed = False
+        db_table = 'auth_user_user_permissions'
+        unique_together = (('user', 'permission'),)
+
+
+class DjangoAdminLog(models.Model):
+    action_time = models.DateTimeField()
+    object_id = models.TextField(blank=True, null=True)
+    object_repr = models.CharField(max_length=200)
+    action_flag = models.PositiveSmallIntegerField()
+    change_message = models.TextField()
+    content_type = models.ForeignKey('DjangoContentType', models.DO_NOTHING, blank=True, null=True)
+    user = models.ForeignKey(AuthUser, models.DO_NOTHING)
+
+    class Meta:
+        managed = False
+        db_table = 'django_admin_log'
+
+
+class DjangoContentType(models.Model):
+    app_label = models.CharField(max_length=100)
+    model = models.CharField(max_length=100)
+
+    class Meta:
+        managed = False
+        db_table = 'django_content_type'
+        unique_together = (('app_label', 'model'),)
+
+
+class DjangoMigrations(models.Model):
+    app = models.CharField(max_length=255)
+    name = models.CharField(max_length=255)
+    applied = models.DateTimeField()
+
+    class Meta:
+        managed = False
+        db_table = 'django_migrations'
+
+
+class DjangoSession(models.Model):
+    session_key = models.CharField(primary_key=True, max_length=40)
+    session_data = models.TextField()
+    expire_date = models.DateTimeField()
+
+    class Meta:
+        managed = False
+        db_table = 'django_session'
 
 
 class Landlord(models.Model):
-    landlord_phone = models.CharField(verbose_name="房东手机号",max_length=13,null=True)
-    house_number = models.CharField(verbose_name="出租门牌号",max_length=255,null=True)
-    landlord_name = models.CharField(verbose_name="房东姓名",db_column='Landlord_name', max_length=255,null=True)  # Field name made lowercase.
+    landlord_phone = models.CharField(max_length=13,verbose_name='房东手机号')
+    house_number = models.CharField(max_length=255, verbose_name='门牌号')
+    landlord_name = models.CharField(db_column='Landlord_name', max_length=255, verbose_name='房东姓名')  # Field name made lowercase.
+    password = models.CharField(max_length=8, verbose_name='登录密码')
 
     class Meta:
         managed = True
@@ -20,15 +131,14 @@ class Landlord(models.Model):
 
 
 class Money(models.Model):
-    # id = models.IntegerField(primary_key=True)
-    house_number = models.CharField(verbose_name="门牌号",max_length=255,null=True)
-    room_num = models.CharField(verbose_name="房间号",max_length=5,null=True)
-    rent_time = models.DateField(verbose_name="记录时间",null=True)
-    water_content = models.FloatField(verbose_name="用水量",null=True)
-    kwh = models.FloatField(verbose_name="用电量",null=True)
-    room_price = models.DecimalField(verbose_name="房租",max_digits=10, decimal_places=2,null=True)
-    other = models.DecimalField(verbose_name="其他",max_digits=10, decimal_places=2, blank=True, null=True)
-    total = models.DecimalField(verbose_name="总计",max_digits=10, decimal_places=2,null=True)
+    house_number = models.CharField(max_length=255, verbose_name='门牌号')
+    room_num = models.CharField(max_length=5,verbose_name='房间号')
+    rent_time = models.DateField(verbose_name='出租时间')
+    water_content = models.FloatField(verbose_name='当前用水量')
+    kwh = models.FloatField(verbose_name='当前用电量')
+    room_price = models.DecimalField(max_digits=10, decimal_places=2, blank=True, null=True, verbose_name='租金')
+    other = models.DecimalField(max_digits=10, decimal_places=2, blank=True, null=True, verbose_name='其他')
+    total = models.DecimalField(max_digits=10, decimal_places=2, verbose_name='总计')
 
     class Meta:
         managed = True
@@ -36,12 +146,12 @@ class Money(models.Model):
 
 
 class Room(models.Model):
-    house_number = models.CharField(verbose_name="门牌号",max_length=255,null=True)
-    water_price = models.DecimalField(verbose_name="水费",max_digits=10, decimal_places=2,null=True)
-    power_price = models.DecimalField(verbose_name="电费",max_digits=10, decimal_places=2,null=True)
-    room_price = models.CharField(verbose_name="房租",max_length=11,null=True)
-    deposit = models.CharField(verbose_name="押金",max_length=11,null=True)
-    room_num = models.CharField(verbose_name="房间号",max_length=255,null=True)
+    house_number = models.CharField(max_length=255, verbose_name='门牌号')
+    water_price = models.DecimalField(max_digits=10, decimal_places=2,verbose_name='电费')
+    power_price = models.DecimalField(max_digits=10, decimal_places=2,verbose_name='水费')
+    room_price = models.CharField(max_length=11,verbose_name='租金')
+    deposit = models.CharField(max_length=11,verbose_name='押金')
+    room_num = models.CharField(max_length=255,verbose_name='房间号')
 
     class Meta:
         managed = True
@@ -49,17 +159,17 @@ class Room(models.Model):
 
 
 class User(models.Model):
-    house_number = models.CharField(verbose_name="门牌号",max_length=255,null=True)
-    room_num = models.CharField(verbose_name="房间号",max_length=5,null=True)
-    name = models.CharField(verbose_name="姓名",max_length=16,null=True)
-    gender = models.CharField(verbose_name="性别",max_length=6,null=True)
-    age = models.CharField(verbose_name="年龄",max_length=6,default="00")
-    tel = models.CharField(verbose_name="手机号",max_length=13,null=True)
-    id_card = models.BigIntegerField(verbose_name="身份证",null=True)
-    job = models.CharField(verbose_name="工作",max_length=255,null=True)
-    address = models.CharField(verbose_name="籍贯",max_length=255,null=True)
-    check_in_time = models.DateField(verbose_name="入住时间",db_column='check-in_time')  # Field renamed to remove unsuitable characters.
-    leave_time = models.DateField(verbose_name="搬离时间",blank=True, null=True)
+    house_number = models.CharField(max_length=255,verbose_name='门牌号')
+    room_num = models.CharField(max_length=5,verbose_name='房间号')
+    name = models.CharField(max_length=16, verbose_name='用户姓名')
+    gender = models.CharField(max_length=6, verbose_name='性别')
+    age = models.CharField(max_length=6, verbose_name='年龄')
+    tel = models.CharField(max_length=13,verbose_name='手机号')
+    id_card = models.BigIntegerField(verbose_name='身份证')
+    job = models.CharField(max_length=255, verbose_name='工作')
+    address = models.CharField(max_length=255,verbose_name='地址')
+    check_in_time = models.DateTimeField(db_column='check-in_time', verbose_name='入住时间')  # Field renamed to remove unsuitable characters.
+    leave_time = models.DateField(blank=True, null=True, verbose_name='退租时间')
 
     class Meta:
         managed = True
--- webAdmin/views/index.py
+++ webAdmin/views/index.py
@@ -1,14 +1,39 @@
-import json
 
-import requests
 from django.shortcuts import render, redirect
-from django.urls import reverse
-
+from django.forms import ModelForm
 from django.http import HttpResponse
+
+from user_massage.models import Landlord
 # Create your views here.
 
+class LandlordModelForm(ModelForm):
+    class Meta:
+        model = Landlord
+        fields = "__all__"
+
 def index(request):
-    return render(request,'rentPc/index/index.html')
+    
+    if request.method =="POST":
+        form= LandlordModelForm()
+        tel= request.POST.get("tel")
+        password= request.POST.get("password")
+        land_lord=Landlord.objects.filter(landlord_phone=tel, password=password).first()
+        if land_lord:
+            h_name=land_lord.house_number
+            return render(request,'rentPc/index/index.html',{"name":h_name})
+
+        return render(request,'rentPc/index/login.html',{"msg":"手机号/密码错误","form":form})
+
+    return HttpResponse("请先登录~")
 
 def login(request):
-    return render(request, 'rentPc/index/login.html')
+
+    form= LandlordModelForm()
+    if request.method =="GET":
+        return render(request, 'rentPc/index/login.html',{"form":form})
+    form= LandlordModelForm(data=request.POST)
+    if form.is_valid():
+        form.save()
+        return render(request,'rentPc/index/login.html',{"msg_t":"注册成功!","form":form})
+    return render(request, 'rentPc/index/login.html',{"form":form})
+    
